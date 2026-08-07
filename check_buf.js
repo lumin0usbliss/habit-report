c0140f3a9287208624fbd1dbe4f8968f0b4371e5
lumin0usbliss <liso241215@gmail.com>
2026-08-08 00:12:22 +0900

    chore: remove temp debug files

--- check_buf.js
+++
@@ -1,3 +0,0 @@
-﻿const fs = require('fs');
-const buffer = fs.readFileSync('transcript_p09.jsonl');
-console.log(buffer.slice(0, 50));
--- fetch-err.js
+++
@@ -1,6 +0,0 @@
-﻿const http = require('http');
-http.get('http://localhost:3000/result/123', (resp) => {
-  let data = '';
-  resp.on('data', (chunk) => { data += chunk; });
-  resp.on('end', () => { console.log('STATUS:', resp.statusCode); console.log(data.substring(0, 500)); });
-}).on('error', (err) => { console.log('Error: ' + err.message); });
